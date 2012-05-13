b90dd97ce93a7989b99f3fcf642ff8b2fd240218
fengclient <fengclient@gmail.com>
2012-05-13 20:56:14 +0800

    real multiple threading

--- pyalgorithms/src/scripts/benchmark.py
+++ pyalgorithms/src/scripts/benchmark.py
@@ -4,12 +4,12 @@ Created on 2012-5-7
 
 @author: fengclient
 '''
-from urllib2 import urlopen
+from urllib2 import urlopen,HTTPError
 from time import time
 import random
 random.seed(time())
 from StringIO import StringIO
-from threading import Timer,Thread
+from threading import Timer,Thread,Event,Semaphore
 
 #perf
 #concurrency sched module
@@ -37,22 +37,22 @@ def response(count=100,cocurr=1):
         time_sum+=(end-start)
     return time_sum/count
 
-flag=True
-counter=[]
-threads=[]
+def stop(stop_flag):
+    stop_flag.set()
+    #print 'stop_flag is set'
 
-def stop():
-    global flag
-    flag=False
-    #print 'stop flag is set'
-
-def run(counter,index):
-    global flag,domain
-    while flag:
-        print 'thread(%d),count=%d'%(index,counter[index])
-        urlopen(r'http://s.com/convert?orig=http://%s/%d/%d'%(domain,index,counter[index]))
-        counter[index]=counter[index]+1
+def run(req_counter,err_counter,index,stop_flag,pool_threads):
+    pool_threads.acquire()
+    while not stop_flag.is_set():
+        #print 'thread(%d),count=%d'%(index,req_counter[index])
+        try:
+            urlopen(r'http://s.com/convert?orig=http://%s/%d/%d'%(domain,index,req_counter[index]))
+        except HTTPError as e:
+            #print 'http exception is catched:',e
+            err_counter[index]=err_counter[index]+1
+        req_counter[index]=req_counter[index]+1
     #print 'thread(%d) is stopped'%(index)
+    pool_threads.release()
 
 def throughput(seconds=10,cocurrency=1):
     '''
@@ -60,32 +60,55 @@ def throughput(seconds=10,cocurrency=1):
     1806 with 10 user
     1485 with 2 user
     '''
-    global counter,threads
-    t=Timer(seconds,stop)
+    stop_flag=Event()
+    pool_threads=Semaphore(cocurrency)
+    req_counter=[]
+    err_counter=[]
+    threads=[]
+    t=Timer(seconds,stop,args=[stop_flag])
     for i in range(cocurrency):
-        counter.append(0)
-        threads.append(Thread(target=run,args=(counter,i)))
+        req_counter.append(0)
+        err_counter.append(0)
+        threads.append(Thread(target=run,args=(req_counter,err_counter,i,stop_flag,pool_threads)))
     t.start()
     for thread in threads:
         thread.start()
+    #print 'start waiting for workers'
+    i=0
+    while i<cocurrency and pool_threads.acquire():
+        #print 'one worker is finished'
+        i=i+1
+    return sum(req_counter),sum(err_counter)
 
 def throughput_singlethread(seconds=10):
     '''
     i got 844 in 60s?
     '''
-    global flag,domain
-    t=Timer(seconds,stop)
+    stop_event=Event()
+    t=Timer(seconds,stop_event)
     t.start()
     count=0
-    while flag:
-        print 'count=%d'%(count)
+    while not stop_event.is_set():
+        #print 'count=%d'%(count)
         urlopen(r'http://s.com/convert?orig=http://%s/%d'%(domain,count))
         count=count+1
-    print 'stopped'
+    #print 'execution is stopped'
     return count
+
+def reset():
+    return urlopen(r'http://s.com/reset')
 
 if __name__ == '__main__':
     #print 'response time:',response(10)
-    print 'throughput time:',throughput(60,2),'per %d seconds'%(60)
-    #print 'throughput time:',throughput_singlethread(60),'per %d seconds'%(60)
+    test_pairs=[(10,1),(10,2),(10,3),(10,4),(10,5),
+                (10,6),(10,7),(10,8),(10,9),(10,10)]
+    for seconds,users in test_pairs:
+        reset()
+        ret=throughput(seconds,users)
+        print 'throughput in %ds with %d users: %d requests with %d errors, fail rate=%f'\
+                %(seconds,users,ret[0],ret[1],float(ret[1])/ret[0])
+
+#    ret=throughput(600,3)
+#    print 'throughput in %ds with %d users: %d requests with %d errors, fail rate=%f'\
+#                %(600,3,ret[0],ret[1],float(ret[1])/ret[0])
     
