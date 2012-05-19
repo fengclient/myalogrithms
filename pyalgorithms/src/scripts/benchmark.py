b1a8e7cd2783e0eec1678878e2523e5a392d2cae
fengclient <fengclient@gmail.com>
2012-05-19 10:29:08 +0800

    simple is beauty

--- pyalgorithms/src/scripts/benchmark.py
+++ pyalgorithms/src/scripts/benchmark.py
@@ -10,6 +10,7 @@ import random
 random.seed(time())
 from StringIO import StringIO
 from threading import Timer,Thread,Event,Semaphore
+from argparse import ArgumentParser
 
 #perf
 #concurrency sched module
@@ -34,81 +35,108 @@ def response(count=100,cocurr=1):
         start=time()
         urlopen(r'http://s.com/convert?orig=http://%s/%s'%(domain,i))
         end=time()
-        time_sum+=(end-start)
+        time_sum=time_sum+(end-start)
     return time_sum/count
 
 def stop(stop_flag):
     stop_flag.set()
     #print 'stop_flag is set'
 
-def run(req_counter,err_counter,index,stop_flag,pool_threads):
-    pool_threads.acquire()
+def run(req_counter,err_counter,time_cost,index,stop_flag):
     while not stop_flag.is_set():
         #print 'thread(%d),count=%d'%(index,req_counter[index])
+        start=time()
         try:
             urlopen(r'http://s.com/convert?orig=http://%s/%d/%d'%(domain,index,req_counter[index]))
         except HTTPError as e:
             #print 'http exception is catched:',e
             err_counter[index]=err_counter[index]+1
+        end=time()
+        time_cost[index]=time_cost[index]+(end-start)
         req_counter[index]=req_counter[index]+1
     #print 'thread(%d) is stopped'%(index)
-    pool_threads.release()
 
 def throughput(seconds=10,cocurrency=1):
     '''
-    i got 808 in 60s with 1 user;
-    1806 with 10 user
-    1485 with 2 user
+    seconds should be greater than or equal to 10
+    1000w pv = 115 rps
     '''
     stop_flag=Event()
-    pool_threads=Semaphore(cocurrency)
     req_counter=[]
     err_counter=[]
+    time_cost=[]
     threads=[]
     t=Timer(seconds,stop,args=[stop_flag])
     for i in range(cocurrency):
         req_counter.append(0)
         err_counter.append(0)
-        threads.append(Thread(target=run,args=(req_counter,err_counter,i,stop_flag,pool_threads)))
+        time_cost.append(0)
+        threads.append(Thread(target=run,args=(req_counter,err_counter,time_cost,i,stop_flag)))
     t.start()
     for thread in threads:
         thread.start()
-    #print 'start waiting for workers'
-    i=0
-    while i<cocurrency and pool_threads.acquire():
-        #print 'one worker is finished'
-        i=i+1
-    return sum(req_counter),sum(err_counter)
-
-def throughput_singlethread(seconds=10):
-    '''
-    i got 844 in 60s?
-    '''
-    stop_event=Event()
-    t=Timer(seconds,stop_event)
-    t.start()
-    count=0
-    while not stop_event.is_set():
-        #print 'count=%d'%(count)
-        urlopen(r'http://s.com/convert?orig=http://%s/%d'%(domain,count))
-        count=count+1
-    #print 'execution is stopped'
-    return count
+    #print 'start waiting for workers:',len(threads)
+    stop_flag.wait()
+    for t in threads:
+        t.join()
+    total=sum(req_counter)
+    err=sum(err_counter)
+    cost=sum(time_cost)/total if total>0 else 0
+    return total,err,cost
 
 def reset():
     return urlopen(r'http://s.com/reset')
 
-if __name__ == '__main__':
-    #print 'response time:',response(10)
-    test_pairs=[(10,1),(10,2),(10,3),(10,4),(10,5),
-                (10,6),(10,7),(10,8),(10,9),(10,10)]
-    for seconds,users in test_pairs:
-        reset()
-        ret=throughput(seconds,users)
-        print 'throughput in %ds with %d users: %d requests with %d errors, fail rate=%f'\
-                %(seconds,users,ret[0],ret[1],float(ret[1])/ret[0])
+def extract_exec_time_from_access_log(path):
+    '''
+    used to extract average execution time from microtime data in apache access file
+    '''
+    detail=[]
+    with open(path) as f:
+        line=f.readline()
+        while line:
+            cost = float(line.split(' ')[0]) / 10000
+            detail.append(cost)
+            line=f.readline()
+    return detail
 
-#    ret=throughput(600,3)
-#    print 'throughput in %ds with %d users: %d requests with %d errors, fail rate=%f'\
-#                %(600,3,ret[0],ret[1],float(ret[1])/ret[0])
-    
+def populate(count=100000):
+    '''
+    populate records
+    '''
+    import sys
+    sys.path.append(r'D:\Workbench\private\web2py_src\applications\welcome\models')
+    db=__import__('db')
+    i=0
+    while i<count:
+        db.db.orig_to_short.insert(originalurl=r'http://%s/%d'%(domain,i),shorturl=repr(i))
+        if i%10000==0:
+            db.db.commit()
+            print '%d is reached'%(i)
+        i=i+1
+    db.db.commit()
+    print '%d is finished'%(i)
+
+def take_benchmark():
+    test_pairs=[(10,1),(10,2),(10,3),(10,4),(10,5),(10,6),(10,7),(10,8),(10,9),(10,10)]
+    print '--------------------------thoughtput test on s.com---------------------------'
+    print 'time\tusers\trequests\terrors\taver_resp_time\tfail-rate'
+    for seconds,users in test_pairs:
+        ret=throughput(seconds,users)
+        print '%d\t%d\t%d\t%d\t%f\t%f'%(seconds,users,ret[0],ret[1],ret[2],float(ret[1])/ret[0] if ret[0]>0 else 0)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description = 'benchmark')
+    parser.add_argument('-b','--benchmark', action='store_true',help= 'take benchmark')
+    parser.add_argument('-po','--populate', type=int,metavar='<count>',help= 'populate databases')
+    parser.add_argument('-p','--parselog', metavar='<file>',help='extract average execution time from microtime data in apache access log file')
+    ns = parser.parse_args()
+    if ns.benchmark:
+        take_benchmark()
+    elif ns.populate:
+        populate(ns.populate)
+    elif ns.parselog:
+        details=extract_exec_time_from_access_log(ns.parselog)
+        print 'count=%d,average=%f'%(len(details),sum(details)/len(details))
+    else:
+        print 'args error?'
