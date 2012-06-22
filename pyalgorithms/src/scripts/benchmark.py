43d5cb585701c5914596245a21dde2f3bef49d98
fengclient <fengclient@gmail.com>
2012-06-22 21:50:39 +0800

    use multiprocessing instead of threading. to avoid GIL limitation

--- pyalgorithms/src/scripts/benchmark.py
+++ pyalgorithms/src/scripts/benchmark.py
@@ -9,7 +9,8 @@ from time import time
 import random
 random.seed(time())
 from StringIO import StringIO
-from threading import Timer,Thread,Event,Semaphore
+from threading import Timer
+from multiprocessing import Process,Event,Queue
 from argparse import ArgumentParser
 
 #perf
@@ -42,21 +43,23 @@ def stop(stop_flag):
     stop_flag.set()
     #print 'stop_flag is set'
 
-def run(req_counter,err_counter,time_cost,index,stop_flag):
+def run(msg_queue,stop_flag):
+    req_counter=err_counter=time_cost=0
     while not stop_flag.is_set():
         #print 'thread(%d),count=%d'%(index,req_counter[index])
         start=time()
         try:
             #urlopen(r'http://s.com/convert?orig=http://%s/%d/%d'%(domain,index,req_counter[index]))
-            urlopen(r'http://s.com/40236')
+            #urlopen(r'http://s.com/40236')
+            urlopen(r'http://www.baidu.com/')
         except HTTPError as e:
-            err_counter[index]=err_counter[index]+1
+            err_counter=err_counter+1
         except URLError as e:
-            err_counter[index]=err_counter[index]+1
+            err_counter=err_counter+1
         end=time()
-        time_cost[index]=time_cost[index]+(end-start)
-        req_counter[index]=req_counter[index]+1
-    #print 'thread(%d) is stopped'%(index)
+        time_cost=time_cost+(end-start)
+        req_counter=req_counter+1
+    msg_queue.put((req_counter,err_counter,time_cost))
 
 def throughput(seconds=10,cocurrency=1):
     '''
@@ -64,26 +67,26 @@ def throughput(seconds=10,cocurrency=1):
     1000w pv = 115 rps
     '''
     stop_flag=Event()
-    req_counter=[]
-    err_counter=[]
-    time_cost=[]
-    threads=[]
+    processes=[]
     t=Timer(seconds,stop,args=[stop_flag])
+    q = Queue()
     for i in range(cocurrency):
-        req_counter.append(0)
-        err_counter.append(0)
-        time_cost.append(0)
-        threads.append(Thread(target=run,args=(req_counter,err_counter,time_cost,i,stop_flag)))
+        processes.append(Process(target=run,args=(q,stop_flag)))
     t.start()
-    for thread in threads:
-        thread.start()
-    #print 'start waiting for workers:',len(threads)
+    for p in processes:
+        p.start()
+    #print 'start waiting for workers:',len(processes)
     stop_flag.wait()
-    for t in threads:
+    for t in processes:
         t.join()
-    total=sum(req_counter)
-    err=sum(err_counter)
-    cost=sum(time_cost)/total if total>0 else 0
+    total=err=cost=0
+    while not q.empty():
+        (req_counter,err_counter,time_cost)=q.get()
+        total=total+req_counter
+        err=err+err_counter
+        cost=cost+time_cost
+    cost=cost/total if total>0 else 0
+
     return total,err,cost
 
 def reset():
