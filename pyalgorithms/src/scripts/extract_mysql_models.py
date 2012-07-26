c0447d7b5c520ce12960ed49144d631e77b7741b
fengclient <fengclient@gmail.com>
2012-07-26 16:15:17 +0800

    fall back to linux version and add primary key support

--- pyalgorithms/src/scripts/extract_mysql_models.py
+++ pyalgorithms/src/scripts/extract_mysql_models.py
@@ -12,8 +12,6 @@ Under Windows you will probably need to add the mysql executable directory to th
 you will also need to modify mysql to mysql.exe and mysqldump to mysqldump.exe below.
 Just guessing here :)
 
-Modified: quick comment on windows platform: assign universal_newlines = True where popen() is used
-
 Access your tables with:
 legacy_db(legacy_db.mytable.id>0).select()
 
@@ -21,6 +19,11 @@ If the script crashes this is might be due to that fact that the data_type_map d
 Please complete it, improve it and continue.
 
 Created by Falko Krause, minor modifications by Massimo Di Pierro and Ron McOuat
+
+2012-07-26: 
+Changed by xiaoftang: 
+1 force protocol=tcp on mysql and mysqldump
+2 set DAL options
 '''
 import subprocess
 import re
@@ -53,51 +56,31 @@ data_type_map = dict(
         longtext = 'text',
         )
 
-class table_def(object):
-    '''
-    table definition
-    '''
-    table_name=None
-    db_codec=None
-    columns=[]
-
-class column_def(object):
-    '''
-    column definition
-    '''
-    Field=None
-    Type=None
-    Null=None
-    Key=None
-    Default=None
-    Extra=None
-    #Constant=None    
-
 def mysql(database_name, username, password):
+    print 'from gluon.dal import DAL, Field'
     p = subprocess.Popen(['mysql',
+                          '--protocol=tcp',
                           '--user=%s' % username,
                           '--password=%s'% password,
                           '--execute=show tables;',
                           database_name],
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         universal_newlines=True)
+                         stderr=subprocess.PIPE)
     sql_showtables, stderr = p.communicate()
     tables = [re.sub('\|\s+([^\|*])\s+.*', '\1', x) for x in sql_showtables.split()[1:]]
-    connection_string = "legacy_db = DAL('mysql://%s:%s@localhost/%s')"%(username, password, database_name)
+    connection_string = "legacy_db = DAL('mysql://%s:%s@localhost/%s',pool_size=10, db_codec='latin1',migrate=False, migrate_enabled=False)"\
+                        %(username, password, database_name)
     legacy_db_table_web2py_code = []
     for table_name in tables:
         #get the sql create statement
         p = subprocess.Popen(['mysqldump',
+                              '--protocol=tcp',
                               '--user=%s' % username,
                               '--password=%s' % password,
                               '--skip-add-drop-table',
                               '--no-data', database_name,
-                              table_name], stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE,
-                              universal_newlines=True)
+                              table_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         sql_create_stmnt,stderr = p.communicate()
         if 'CREATE' in sql_create_stmnt:#check if the table exists
             #remove garbage lines from sql statement
@@ -106,10 +89,14 @@ def mysql(database_name, username, password):
             #generate the web2py code from the create statement
             web2py_table_code = ''
             table_name = re.search('CREATE TABLE .(\S+). \(', sql_lines[0]).group(1)
-            table=table_def()
-            table.table_name=table_name
-            fields = table.columns
+            fields = []
+            auto_increment_fields = []
+            primary_key_fields = []
+            id_field = None
             for line in sql_lines[1:-1]:
+                hit_pk=re.search('PRIMARY KEY \((\S+)\)', line)
+                if hit_pk:
+                    primary_key_fields=re.findall('`(\S+?)`', line)
                 if re.search('KEY', line) or re.search('PRIMARY', line) or re.search(' ID', line) or line.startswith(')'):
                     continue
                 hit = re.search('(\S+)\s+(\S+)(,| )( .*)?', line)
@@ -117,7 +104,18 @@ def mysql(database_name, username, password):
                     name, d_type = hit.group(1), hit.group(2)
                     d_type = re.sub(r'(\w+)\(.*',r'\1',d_type)
                     name = re.sub('`','',name)
+                    if re.search('AUTO_INCREMENT',line):
+                        auto_increment_fields.append(name)
+                    fields.append((name,d_type))
+            for (name, d_type) in fields:
+                if name in primary_key_fields and name in auto_increment_fields and len(primary_key_fields)==1:
+                    id_field = name
+                    #i don't care d_type
+                    web2py_table_code += "\n    Field('%s','%s'),"%(name,'id')
+                else:
                     web2py_table_code += "\n    Field('%s','%s'),"%(name,data_type_map[d_type])
+            if primary_key_fields:
+                web2py_table_code += "\n    primarykey=[%s],"%(','.join(["'%s'"%(x) for x in primary_key_fields]))
             web2py_table_code = "legacy_db.define_table('%s',%s\n    migrate=False)"%(table_name,web2py_table_code)
             legacy_db_table_web2py_code.append(web2py_table_code)
     #----------------------------------------
